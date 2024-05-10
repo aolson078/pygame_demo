ffa0c29ebb2d68cb55b230b82c930a36b14109fc
Alexander Olson <aolson078@gmail.com>
2024-05-10 09:30:51 -0400

    Added walking animations

--- static/sprites/sprites.py
+++ static/sprites/sprites.py
@@ -30,6 +30,7 @@ class Player(pygame.sprite.Sprite):
 		self.y_change = 0
 
 		self.facing = 'down'
+		self.animation_loop = 1
 
 		# image_to_load = pygame.image.load('static/assets/images/alien.png')
 		# self.image = image_to_load
@@ -51,6 +52,7 @@ class Player(pygame.sprite.Sprite):
 
 	def update(self):
 		self.movement()
+		self.animate()
 
 		self.rect.x += self.x_change
 		self.collide_blocks("x")
@@ -79,7 +81,6 @@ class Player(pygame.sprite.Sprite):
 			self.facing = 'down'
 
 	def collide_blocks(self, direction):
-		print(self.game.blocks)
 		if direction == "x":
 			# checks if rect of one sprite hits rect of other
 			# params: sprite 1, sprite 2, delete on collision?
@@ -100,6 +101,60 @@ class Player(pygame.sprite.Sprite):
 					self.rect.y = hits[0].rect.bottom
 
 
+	def animate(self):
+		down_animations = [self.game.character_spritesheet.get_sprite(3, 2, self.width, self.height),
+		                   self.game.character_spritesheet.get_sprite(35, 2, self.width, self.height),
+		                   self.game.character_spritesheet.get_sprite(68, 2, self.width, self.height)]
+
+		up_animations = [self.game.character_spritesheet.get_sprite(3, 34, self.width, self.height),
+		                 self.game.character_spritesheet.get_sprite(35, 34, self.width, self.height),
+		                 self.game.character_spritesheet.get_sprite(68, 34, self.width, self.height)]
+
+		left_animations = [self.game.character_spritesheet.get_sprite(3, 98, self.width, self.height),
+		                   self.game.character_spritesheet.get_sprite(35, 98, self.width, self.height),
+		                   self.game.character_spritesheet.get_sprite(68, 98, self.width, self.height)]
+
+		right_animations = [self.game.character_spritesheet.get_sprite(3, 66, self.width, self.height),
+		                    self.game.character_spritesheet.get_sprite(35, 66, self.width, self.height),
+		                    self.game.character_spritesheet.get_sprite(68, 66, self.width, self.height)]
+
+
+		# if were standing still, set image to static. If y change isnt 0, we are moving, and using down_animations
+		if self.facing == "down":
+			if self.y_change == 0:
+				self.image = self.game.character_spritesheet.get_sprite(3, 2, self.width, self.height)
+			else:
+				self.image = down_animations[math.floor(self.animation_loop)]
+				self.animation_loop += 0.1
+				if self.animation_loop >= 3:
+					self.animation_loop = 1
+
+		if self.facing == "up":
+			if self.y_change == 0:
+				self.image = self.game.character_spritesheet.get_sprite(3, 34, self.width, self.height)
+			else:
+				self.image = up_animations[math.floor(self.animation_loop)]
+				self.animation_loop += 0.1
+				if self.animation_loop >= 3:
+					self.animation_loop = 1
+
+		if self.facing == "left":
+			if self.x_change == 0:
+				self.image = self.game.character_spritesheet.get_sprite(3, 98, self.width, self.height)
+			else:
+				self.image = left_animations[math.floor(self.animation_loop)]
+				self.animation_loop += 0.1
+				if self.animation_loop >= 3:
+					self.animation_loop = 1
+
+		if self.facing == "right":
+			if self.x_change == 0:
+				self.image = self.game.character_spritesheet.get_sprite(3, 66, self.width, self.height)
+			else:
+				self.image = right_animations[math.floor(self.animation_loop)]
+				self.animation_loop += 0.1
+				if self.animation_loop >= 3:
+					self.animation_loop = 1
 
 class Block(pygame.sprite.Sprite):
 	def __init__(self, game, x, y):
